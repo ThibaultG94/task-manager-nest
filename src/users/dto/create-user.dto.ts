7345ad98b5f1a280b350cde57ce33bde88bd2291
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-10-23 11:26:16 +0200

    Ajout de la validation des données dans CreateUserDto et mise à jour des dépendances dans package.json

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(3)
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    password: string;
+
+    @IsOptional()
+    @IsString()
+    avatar?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    tips?: boolean;
+}
