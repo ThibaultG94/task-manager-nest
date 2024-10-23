3a676ca22bad6905c237ad7440866bae3ec1b10b
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-10-23 12:18:30 +0200

    Ajout de la configuration du module @nestjs/config et intégration de TypeORM pour la connexion à la base de données MariaDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { UserContact } from './users/entities/user-contact.entity';
+import { UserBlocked } from './users/entities/user-blocked.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, UserContact, UserBlocked],
+      synchronize: true, // À mettre à false en production !
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
