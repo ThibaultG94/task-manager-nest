cbb40c260633bd6074b804dc92e32c3f7a784c9e
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-10-25 17:30:24 +0200

    Mise à jour de la configuration de TypeOrm pour utiliser DATABASE_URL et ajout de paramètres de connexion supplémentaires

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,20 @@ import { UserBlocked } from './users/entities/user-blocked.entity';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
       type: 'mariadb',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      // host: process.env.DB_HOST,
+      // port: parseInt(process.env.DB_PORT),
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_DATABASE,
       entities: [User, UserContact, UserBlocked],
       synchronize: process.env.DB_SYNC === 'true',
+      logging: true,
+      connectTimeout: 30000,
+      extra: {
+        connectionLimit: 10
+      }
     }),
     UsersModule
   ],
